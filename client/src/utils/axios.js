570835f6d3ccef2ae3dc02544d245a00dfede5f7
Your Name <you@example.com>
2023-10-15 19:13:49 +0400

    C:/Program Files/Git/api

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import cookie from 'js-cookie'
 const instance = axios.create({
-    baseURL: 'https://posts-server-drab.vercel.app:5000',
+    baseURL: 'https://posts-server-drab.vercel.app:5000/api',
     withCredentials:true
 });
 
